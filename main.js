77f76fd537fd25b5e3373acc5069e75cdeb86e5a
Sabrina L <sabrinali0312@gmail.com>
2019-03-18 19:34:30 -0400

    adding firebase js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-// Initialize Firebase
 var config = {
   apiKey: "AIzaSyCQBL6g30QmHMO_Wblv4Pa15GlYGkk-JIQ",
   authDomain: "gtc-inclass.firebaseapp.com",
@@ -10,20 +9,27 @@ var config = {
 firebase.initializeApp(config);
 
 
-
 $(document).ready(function (){
   var database = firebase.database();
-  $("#submitbtn").on('click',function(){
+  $("#submitbtn").on('click',function(event){
+      event.preventDefault();
       let name = "";
       let role = "";
       let startDate = "";
       let rate = "";
       //read from form and submit to FB
+
       name = $("#employeeName").val().trim();
       role = $("#role").val().trim();
       startDate = $("#startDate").val().trim();
       rate = $("#monthlyRate").val().trim();
 
+      database.ref().push({
+        name:"name",
+        role:"role",
+        startDate:"startDate",
+        rate:"rate"
+      })
 
   })
 })
