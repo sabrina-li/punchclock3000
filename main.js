3e9771a1692cf2f4ef9bc552abd108cc65f5ee14
Sabrina L <sabrinali0312@gmail.com>
2019-03-18 19:23:38 -0400

    js

--- main.js
+++ main.js
@@ -1,22 +1,29 @@
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyCQBL6g30QmHMO_Wblv4Pa15GlYGkk-JIQ",
-    authDomain: "gtc-inclass.firebaseapp.com",
-    databaseURL: "https://gtc-inclass.firebaseio.com",
-    projectId: "gtc-inclass",
-    storageBucket: "gtc-inclass.appspot.com",
-    messagingSenderId: "299297782426"
-  };
+  apiKey: "AIzaSyCQBL6g30QmHMO_Wblv4Pa15GlYGkk-JIQ",
+  authDomain: "gtc-inclass.firebaseapp.com",
+  databaseURL: "https://gtc-inclass.firebaseio.com",
+  projectId: "gtc-inclass",
+  storageBucket: "gtc-inclass.appspot.com",
+  messagingSenderId: "299297782426"
+};
 firebase.initializeApp(config);
 
 
 
 $(document).ready(function (){
-    $("#submitbtn").on('click',function(){
-        //read from form and submit to FB
-        let name = $("#employeeName").val().trim();
-        let role = $("#role").val().trim();
-        let startDate = $("#startDate").val().trim();
-        let rate = $("#monthlyRate").val().trim();
-    })
+  var database = firebase.database();
+  $("#submitbtn").on('click',function(){
+      let name = "";
+      let role = "";
+      let startDate = "";
+      let rate = "";
+      //read from form and submit to FB
+      name = $("#employeeName").val().trim();
+      role = $("#role").val().trim();
+      startDate = $("#startDate").val().trim();
+      rate = $("#monthlyRate").val().trim();
+
+
+  })
 })
