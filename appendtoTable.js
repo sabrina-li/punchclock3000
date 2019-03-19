c046b8f30fdbb7d1c8c53b9a8d518db6c68d5611
Sabrina L <sabrinali0312@gmail.com>
2019-03-18 20:11:21 -0400

    changed email field to name

---
+++ appendtoTable.js
@@ -0,0 +1,25 @@
+function appendtotable(emp){
+    var row = `div class="row form-group">
+                    <div class="col col-sm-2">
+                    <div class="title_text">${emp.name} }</div>
+                    </div>
+                    <div class="col col-sm-2">
+                    <div class="title_text">${emp.email}</div>
+                    </div>
+                    <div class="col col-sm-2">
+                    <div class="title_text">Start Date</div>
+                    </div>
+                    <div class="col col-sm-2">
+                    <div class="title_text">Months Worked</div>
+                    </div>
+                    <div class="col col-sm-2">
+                    <div class="title_text">Monthly Rate ($)</div>
+                    </div>
+                    <div class="col col-sm-2">
+                    <div class="title_text">Total Billed</div>
+                    </div>
+
+                </div>`
+}
+
+//childSnapshot.val()
